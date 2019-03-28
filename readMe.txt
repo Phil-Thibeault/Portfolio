a5a5304cd25ce393e6f6ce0827535677dc9ce99b
Phil-Thibeault <philippe.thibeault99@outlook.com>
2019-03-27 23:01:46 -0400

    read me

---
+++ readMe.txt
@@ -0,0 +1,5 @@
+Ceci est le dépôt contenant tout le code source pour le Portfolio de Philippe Thibeault, finissant en Techniques d'Intégration Multimédia.
+
+Le code est groupé en sous-dossiers (css, js).
+
+Les médias nécessaires sont tous dans le dossier media.
--- test/js/changeBackground.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-	var imgBackground = $('.img-link').css('background-image');
-	console.log(imgBackground);
-	$('.img-link').mouseover(function(){
-    	$(this).css('background', 'green');
-	}).mouseout(function(){
-    	$(this).css('background', imgBackground);
-	});
-});
